c4492e3d48a83adc54ff46788c69c9626bee9c16
2nafish117 <shashank.shashank.c@gmail.com>
2022-06-07 22:27:23 +0530

    add flying mechanics

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class PlayerShip : MonoBehaviour
 {
+	public float MoveAcceleration = 1000.0f;
+	public float RotationSpeed = 6.0f;
+
 	private Rigidbody rigidBody;
 	private Vector3 movement = Vector3.zero;
+	private GameObject pivot = null;
+
+	private Quaternion maxRightRotation = new Quaternion(0, 0, -0.216439515f, 0.976296067f);
+	private Quaternion maxLeftRotation = new Quaternion(0, 0, 0.216439515f, 0.976296067f);
+	
+	private Quaternion maxUpRotation = new Quaternion(-0.207911685f, 0, 0, 0.978147626f);
+	private Quaternion maxDownRotation = new Quaternion(0.207911685f, 0, 0, 0.978147626f);
+
+	private Quaternion defaultRotation = new Quaternion(0, 0, 0, 1);
 
 	private void Start()
 	{
 		rigidBody = GetComponent<Rigidbody>();
+		pivot = transform.GetChild(0).gameObject;
 	}
 	private void Update()
 	{
@@ -32,10 +45,47 @@ public class PlayerShip : MonoBehaviour
 		}
 
 		movement.Normalize();
+		
+		if(movement.x > 0)
+		{
+			Quaternion rot = pivot.transform.rotation;
+			rot = Quaternion.Slerp(rot, maxRightRotation, RotationSpeed * Time.deltaTime);
+			pivot.transform.rotation = rot;
+		}
+		else if(movement.x < 0)
+		{
+			Quaternion rot = pivot.transform.rotation;
+			rot = Quaternion.Slerp(rot, maxLeftRotation, RotationSpeed * Time.deltaTime);
+			pivot.transform.rotation = rot;
+		} else
+		{
+			Quaternion rot = pivot.transform.rotation;
+			rot = Quaternion.Slerp(rot, defaultRotation, RotationSpeed * Time.deltaTime);
+			pivot.transform.rotation = rot;
+		}
+
+		if (movement.y > 0)
+		{
+			Quaternion rot = pivot.transform.rotation;
+			rot = Quaternion.Slerp(rot, maxUpRotation, RotationSpeed * Time.deltaTime);
+			pivot.transform.rotation = rot;
+		}
+		else if (movement.y < 0)
+		{
+			Quaternion rot = pivot.transform.rotation;
+			rot = Quaternion.Slerp(rot, maxDownRotation, RotationSpeed * Time.deltaTime);
+			pivot.transform.rotation = rot;
+		}
+		else
+		{
+			Quaternion rot = pivot.transform.rotation;
+			rot = Quaternion.Slerp(rot, defaultRotation, RotationSpeed * Time.deltaTime);
+			pivot.transform.rotation = rot;
+		}
 	}
 
 	private void FixedUpdate()
 	{
-		rigidBody.AddForce(movement * 1000.0f);
+		rigidBody.AddForce(movement * MoveAcceleration);
 	}
 }
